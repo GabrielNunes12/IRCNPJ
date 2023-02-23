c28a26d7e59d032f4706cf249150c606244ba71e
Gabriel <prismaticnetwork12@gmail.com>
2023-02-22 21:36:06 -0300

    feat: project implemented

---
+++ backend/src/main/java/com/ircnpj/demo/IrcnpjApplication.java
@@ -0,0 +1,13 @@
+package com.ircnpj.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrcnpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrcnpjApplication.class, args);
+	}
+
+}
